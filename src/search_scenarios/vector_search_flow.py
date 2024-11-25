21598a170b05e7034e5786b042aaab6262cddd83
artur.semikov <AG6CBZZ@mmm.com>
2024-11-25 20:07:30 +0200

    [25/11/2024]: added vector search scenario with custom word2vec embedding model

--- src/search_scenarios/vector_search_flow.py
+++ src/search_scenarios/vector_search_flow.py
@@ -1,4 +1,4 @@
-from utils.common_utils import get_search_client, get_search_index_client, extract_key_phrases
+from utils.common_utils import get_search_client, get_search_index_client, extract_key_phrases, bcolors
 from search_queries.search_queries import vector_search
 from gensim_word2vec import gensim_util as gu
 from utils.common_utils import get_text_analytics_client
@@ -17,6 +17,8 @@ if __name__=="__main__":
     key_phrases = extract_key_phrases(prompt,text_analytics_client)
 
     key_phrases_prompt = " ".join(key_phrases)
+
+    print(f"\n{bcolors.BOLD}Searching vectors for prompt: {key_phrases_prompt}\n{bcolors.ENDC}")
 
     vector = gu.get_vector_from_sentence(
         text=key_phrases_prompt,
