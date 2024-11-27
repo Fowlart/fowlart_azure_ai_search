456ae669aa8b9e737b1dad95b508a31e2f72514c
artur.semikov <AG6CBZZ@mmm.com>
2024-11-27 21:27:34 +0200

    [27/11/2024]: extract all environment-specific to one place for further refactoring

--- src/iaac_powershell/create_search_service.ps1
+++ src/iaac_powershell/create_search_service.ps1
@@ -1,1 +1,5 @@
-az search service create --name fowlart-ai-search --resource-group rg-fowlartChat --sku free
+# sku free - no symantic search(use sku basic to explore it)
+az search service create `
+--name fowlart-ai-search `
+--resource-group rg-fowlartChat `
+--sku free
